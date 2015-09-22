e6932f736e39d9ee22975c38a12d164013cf9b02
jrcooke2 <jrcooke2@gmail.com>
2015-09-22 09:21:36 -0700

    Demo

---
+++ Sept22Demo/scripts.js
@@ -0,0 +1,11 @@
+
+/**
+ * Created by inet2005 on 9/22/15.
+ */
+function showMsg()
+{
+    var currentElement = document.body.childNodes[1].childNodes[1];
+
+    currentElement.innerHTML = "Changed Text";
+
+}
