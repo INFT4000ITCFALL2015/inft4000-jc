0e0d54ab938235f945e68899762d256e0c7cb3d8
jrcooke2 <jrcooke2@gmail.com>
2015-09-22 09:42:30 -0700

    Most of Demo, Got behind

--- Sept22Demo/scripts.js
+++ Sept22Demo/scripts.js
@@ -4,8 +4,20 @@
  */
 function showMsg()
 {
-    var currentElement = document.body.childNodes[1].childNodes[1];
+    //This is the very hard way
+    //var currentElement = document.body.childNodes[1].childNodes[1];
+    //currentElement.innerHTML = "Changed Text";
 
-    currentElement.innerHTML = "Changed Text";
+    //best for changing one thing
+    //var currentElement = document.getElementById("headerChange");
+    //currentElement.innerHTML = "Changed Text";
+
+    //changing multiple elements
+    //var headerElementsArray = document.getElementsByTagName("h1");
+   // var i;
+    //for (i = 0; i < headerElementsArray.length; i++){
+        //headerElementsArray[i].innerHTML = "Changed Text";
+    //}
+
 
 }
