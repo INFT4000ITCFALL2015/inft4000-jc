94887a9ecc357700d64541be7e64df232a0333d6
jrcooke2 <jrcooke2@gmail.com>
2015-09-23 08:04:38 -0700

    Started lab1 - Step 1 Complete - Started Step 2

--- Lab1/scripts.js
+++ Lab1/scripts.js
@@ -1,3 +1,14 @@
 /**
  * Created by inet2005 on 9/22/15.
  */
+
+function changeZ1() {
+    document.getElementById("front").style.zIndex = "-1";
+    document.getElementById("back").style.zIndex = "1";
+}
+function changeZ2(){
+    document.getElementById("front").style.zIndex = "1";
+    document.getElementById("back").style.zIndex = "-1";
+}
+
+
