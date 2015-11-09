e0e1ff2ee9b4b89b35e9c39b1d1ebe971bc1d236
jrcooke2 <jrcooke2@gmail.com>
2015-11-09 06:42:18 -0800

    REQ-001 Complete REQ-002 Complete

--- Assignment 2/scripts2.js
+++ Assignment 2/scripts2.js
@@ -1,43 +1,44 @@
 angular.module('studentsApp',[])
-    .controller('studentController',function($scope){
+    .controller('studentController',['$scope', '$http',function($scope, $http) {
 
         $scope.students = []; // end of students array
 
-        $scope.newStudent = function(){
+        $http.get('students.json').success(function (data) {
+            $scope.students = data;
+        });
 
-            $scope.students.push({fName:$scope.firstName, lName:$scope.lastName, stuId:$scope.studentId});
+
+        $scope.newStudent = function () {
+
+            $scope.students.push({fName: $scope.firstName, lName: $scope.lastName, stuId: $scope.studentId});
             $scope.firstName = '';
             $scope.lastName = '';
             $scope.studentId = '';
         };
 
-        $scope.addNewStudent = function(){
+        $scope.addNewStudent = function () {
 
-            if($scope.students.length == 0) {
+            if ($scope.students.length == 0) {
                 $scope.newStudent();
-            }else{
+            } else {
                 var found = false;
-                angular.forEach($scope.students,function(student){
-                        if (student.stuId == $scope.studentId) {
-                            alert("Id has been taken");
-                            found = true;
-                        }
+                angular.forEach($scope.students, function (student) {
+                    if (student.stuId == $scope.studentId) {
+                        alert("Id has been taken");
+                        found = true;
+                    }
                 });
-                if(!found){
+                if (!found) {
                     $scope.newStudent();
                 }
             }
         };
 
-        $scope.showDiv = function(){
+        $scope.showDiv = function () {
             $scope.toShow = true;
         };
 
-        $scope.hideDiv = function(){
+        $scope.hideDiv = function () {
             $scope.toShow = false;
-        }
-
-
-    })
-
-;
+        };
+    }]);
