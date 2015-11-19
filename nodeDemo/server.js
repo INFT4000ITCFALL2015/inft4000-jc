279a0dc3e310884787902793e28b9ed59215e10f
jrcooke2 <jrcooke2@gmail.com>
2015-11-19 05:32:26 -0800

    Stuf

--- nodeDemo/server.js
+++ nodeDemo/server.js
@@ -2,12 +2,35 @@
  * Created by inet2005 on 11/17/15.
  */
 var http = require('http');
+var mysql      = require('mysql');
+var sqlResults;
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'inet2005',
+    database : 'sakila'
+});
+
+connection.connect();
+
+var sql = "SELECT * FROM ?? WHERE ?? = ?";
+var inserts = ['film', 'film_id', 5];
+sql = mysql.format(sql, inserts);
+
+connection.query(sql, function(err, results) {
+    if (err) throw err;
+    sqlResults= results;
+
+});
+
+connection.end();
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end(JSON.stringify(sqlResults));
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
 
-console.log(process.argv[1]);
+
