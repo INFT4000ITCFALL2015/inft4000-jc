4ae4b9b49bc167b3e50297ff8f5e608a969b9ac7
jrcooke2 <jrcooke2@gmail.com>
2015-12-04 10:23:22 -0800

    Lab 6 Complete

--- Lab6/routes/index.js
+++ Lab6/routes/index.js
@@ -5,6 +5,7 @@ var employeeController = require('../controllers/employeeController');
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
 });
 
