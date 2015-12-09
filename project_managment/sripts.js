f4a9e1acea66ddcdd3140d67641215dbba966a80
jrcooke2 <jrcooke2@gmail.com>
2015-12-09 08:18:56 -0800

    Stuff

--- project_managment/sripts.js
+++ project_managment/sripts.js
@@ -3,19 +3,17 @@ angular.module('masterMindApp',[])
 
         $scope.attemptCounter= 0;
 
+
+        $scope.avalibleOption = ['Blue', 'Red', 'Purple', 'Green'];
+
         $scope.data = {
             availableOptions: [
                 {id: 'Red', name: 'Red'},
                 {id: 'Blue', name: 'Blue'},
                 {id: 'Green', name: 'Green'},
-                {id: 'Yellow', name: 'Yellow'}
+                {id: 'Purple', name: 'Purple'}
             ],
-            answerSet:[
-                {c1: 'Blue'},
-                {c2: 'Blue'},
-                {c3: 'Blue'},
-                {c4: 'Blue'}
-            ]
+            answerSet:['','','','']
         };
 
         //Declare patterns Array
@@ -23,20 +21,34 @@ angular.module('masterMindApp',[])
 
         $scope.counterUp = function(){
             $scope.attemptCounter ++;
-            alert($scope.attemptCounter);
+        };
+
+        $scope.getAnswerSet = function(){
+            for(var i = 0; i <= 3; i++) {
+                var index = Math.floor((Math.random() * 3));
+                $scope.data.answerSet[i] = $scope.avalibleOption[index];
+                //alert($scope.data.answerSet[i]);
+            }
         };
 
         //Adding a new pattern to array
         $scope.newColourSet = function () {
             if($scope.attemptCounter > 7){
+                alert('You have lost!')
 
             }else {
                 $scope.colourSets.push({
                     colourOne: $scope.colOne,
                     colourTwo: $scope.colTwo,
                     colourThree: $scope.colThree,
-                    colourFour: $scope.colFour
+                    colourFour: $scope.colFour,
+                    matchedColours: 0,
+                    matchedPosition: 0
                 });
+                $scope.ColourOneFound = 0;
+                $scope.ColourTwoFound = 0;
+                $scope.ColourThreeFound = 0;
+                $scope.ColourFourFound = 0;
                 $scope.checkAns();
                 $scope.counterUp();
             }
@@ -44,20 +56,71 @@ angular.module('masterMindApp',[])
 
         $scope.checkAns = function (){
 
-            var counter = 0;
-            var found = false;
-            //for(var i = 0; i < 5; i++) {
-                if ($scope.colourSets[$scope.attemptCounter].colourOne == $scope.data.answerSet[0].c1) {
-                    alert("c1 match");
-                    found = true;
-                }
-            //}
-            if (!found) {
-                counter += 1;
+            $scope.checkColour();
+            $scope.checkPosition();
+            if($scope.colourSets[$scope.attemptCounter].matchedPosition == 4){
+                alert('Your The Winner, hit Shift F5 to play Again!!!!!');
             }
 
-            return counter;
+        };
 
-        }
+        $scope.checkColour = function (){
+
+            for(var i = 0; i <= 3; i++) {
+                if($scope.ColourOneFound==1){break;}
+                if ($scope.colourSets[$scope.attemptCounter].colourOne == $scope.data.answerSet[i]){
+                    $scope.colourSets[$scope.attemptCounter].matchedColours ++;
+                    $scope.ColourOneFound = 1;
+                }
+            }
+
+            for(var i = 0; i <= 3; i++) {
+                if($scope.ColourTwoFound==1){break;}
+                if ($scope.colourSets[$scope.attemptCounter].colourTwo == $scope.data.answerSet[i]){
+                    $scope.colourSets[$scope.attemptCounter].matchedColours ++;
+                    $scope.ColourTwoFound = 1;
+                }
+            }
+
+            for(var i = 0; i <= 3; i++) {
+                if($scope.ColourThreeFound==1){break;}
+                if ($scope.colourSets[$scope.attemptCounter].colourThree == $scope.data.answerSet[i]){
+                    $scope.colourSets[$scope.attemptCounter].matchedColours ++;
+                    $scope.ColourThreeFound = 1;
+                }
+            }
+
+            for(var i = 0; i <= 3; i++) {
+                if($scope.ColourFourFound==1){break;}
+                if ($scope.colourSets[$scope.attemptCounter].colourFour == $scope.data.answerSet[i]){
+                    $scope.colourSets[$scope.attemptCounter].matchedColours ++;
+                    $scope.ColourFourFound = 1;
+                }
+            }
+
+        };
+
+        $scope.checkPosition = function(){
+
+            //for(var i = 0; i <= 3; i++) {
+                if ($scope.colourSets[$scope.attemptCounter].colourOne == $scope.data.answerSet[0]){
+                    $scope.colourSets[$scope.attemptCounter].matchedPosition ++
+                }
+
+                if ($scope.colourSets[$scope.attemptCounter].colourTwo == $scope.data.answerSet[1]){
+                    $scope.colourSets[$scope.attemptCounter].matchedPosition ++
+                }
+
+                if ($scope.colourSets[$scope.attemptCounter].colourThree == $scope.data.answerSet[2]){
+                    $scope.colourSets[$scope.attemptCounter].matchedPosition ++
+                }
+
+                if ($scope.colourSets[$scope.attemptCounter].colourFour == $scope.data.answerSet[3]) {
+                    $scope.colourSets[$scope.attemptCounter].matchedPosition++
+                }
+
+        };
+
+
 
     }]);
